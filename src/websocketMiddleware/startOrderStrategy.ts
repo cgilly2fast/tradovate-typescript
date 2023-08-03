f4b434cea8e34f45c61a84b2fd2bc9b2d7117d85
Colby Gilbert <colbygilbert95@gmail.com>
2023-08-03 11:57:23 -1000

    Stoploss bug fixes & logs improvements

--- src/websocketMiddleware/startOrderStrategy.ts
+++ src/websocketMiddleware/startOrderStrategy.ts
@@ -39,7 +39,7 @@ export const startOrderStrategy = (state:{[k:string]:any}, action:Action) => {
                 
                  
                 if(id === r.i) {
-                    if(r.status === 200) {
+                    if(r.s === 200) {
                         console.log(`[DevX Trader]: New Order Strategy: Status: ${r.s} Action: ${r.d.orderStrategy.action} Id: ${r.d.orderStrategy.id}` )
                     } else {
                         console.log("[DevX Trader]: Error Order Strategy: "+JSON.stringify(r, null, 2))
